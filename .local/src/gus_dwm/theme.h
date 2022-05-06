78aefb3abbcd2708b6679e84f7c08b743687571f
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-05-06 18:24:50 +0000

    removed redundant files and changed red theme

--- .local/src/gus_dwm/theme.h
+++
@@ -1,5 +0,0 @@
-static const char col_normbg[]       = "#090909";
-static const char col_normbord[]       = "#111111";
-static const char col_normfg[]       = "#bbbbbb";
-static const char col_selfg[]       = "#eeeeee";
-static const char col_main[]        = "#9f1b1b";
--- .local/themes/night_red/dwm.h
+++ .local/themes/night_red/dwm.h
@@ -2,4 +2,4 @@ static const char col_normbg[]       = "#090909";
 static const char col_normbord[]       = "#111111";
 static const char col_normfg[]       = "#bbbbbb";
 static const char col_selfg[]       = "#eeeeee";
-static const char col_main[]        = "#9f1b1b";
+static const char col_main[]        = "#b50000";
