38f24b874c6ddfb19c745f95676fc49c25f0a067
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-05-06 19:05:52 +0000

    changed themes

--- .local/themes/night/dwm.h
+++ .local/themes/night/dwm.h
@@ -1,5 +1,5 @@
 static const char col_normbg[]       = "#090909";
-static const char col_normbord[]       = "#222222";
+static const char col_normbord[]       = "#090909";
 static const char col_normfg[]       = "#bbbbbb";
-static const char col_selfg[]       = "#eeeeee";
-static const char col_main[]        = "#1c62c5";
+static const char col_selfg[]       = "#1c62c5";
+static const char col_main[]        = "#090909";
