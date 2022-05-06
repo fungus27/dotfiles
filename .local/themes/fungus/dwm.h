0487a8bc83c22b5de761afa5628c98d776405081
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-05-06 19:10:35 +0000

    changed themes

--- .local/themes/fungus/dwm.h
+++ .local/themes/fungus/dwm.h
@@ -2,4 +2,4 @@ static const char col_normbg[]       = "#050505";
 static const char col_normbord[]       = "#222222";
 static const char col_normfg[]       = "#eeeeee";
 static const char col_selfg[]       = "#eeeeee";
-static const char col_main[]        = "#8d4ce2";
+static const char col_main[]        = "#6d2dc1";
