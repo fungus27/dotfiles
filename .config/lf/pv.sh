7a6bc9577cdee9ee8c73fd88b4973feed22dc45c
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-05-16 21:28:28 +0000

    added hex preview to lf

---
+++ .config/lf/pv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+case "$(file --dereference --brief --mime-type -- "$1")" in
+    text/*) head -n $3 $1;;
+    *) w=$((($2-10)/6)); hx $1 -c $w -l $(($3 * $w)) -t 1;;
+esac
+#w=$((($2-10)/6))
+#h=$(($3 * $w))
+#hx $1 -c $w -l $h -t 1
