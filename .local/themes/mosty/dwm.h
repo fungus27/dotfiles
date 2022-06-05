36fc62072f1918ed9ff0fa2c70757c7679c91689
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-06-05 11:30:06 +0000

    added 2 themes

---
+++ .local/themes/mosty/dwm.h
@@ -0,0 +1,6 @@
+static const char col_normbg[]       = "#030404";
+static const char col_normbord[]       = "#010202";
+static const char col_normfg[]       = "#aaaca8";
+static const char col_selfg[]       = "#bdbfbb";
+static const char col_selbg[]        = "#161c0b";
+static const char col_selbord[]      = "#161c0b";
---
+++ .local/themes/red_forest/dwm.h
@@ -0,0 +1,6 @@
+static const char col_normbg[]       = "#202020";
+static const char col_normbord[]       = "#121d18";
+static const char col_normfg[]       = "#feb2aa";
+static const char col_selfg[]       = "#eeeeee";
+static const char col_selbg[]        = "#a83334";
+static const char col_selbord[]      = "#b51d2c";
