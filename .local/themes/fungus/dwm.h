55603dc3c0b21e7fad4a4c2c0a66fe12ff2abc62
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-06-05 11:29:17 +0000

    removed unused themes

--- .local/themes/fungus/dwm.h
+++
@@ -1,6 +0,0 @@
-static const char col_normbg[]       = "#050505";
-static const char col_normbord[]       = "#231534";
-static const char col_normfg[]       = "#eeeeee";
-static const char col_selfg[]       = "#eeeeee";
-static const char col_selbg[]        = "#6d2dc1";
-static const char col_selbord[]        = "#6d2dc1";
