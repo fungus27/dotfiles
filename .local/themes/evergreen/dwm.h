9b53d53b6ee240d5c77bcc2dc7560398687c9b69
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-07-26 21:22:31 +0000

    add new theme

---
+++ .local/themes/evergreen/dwm.h
@@ -0,0 +1,6 @@
+static const char col_normbg[]       = "#000000";
+static const char col_normbord[]       = "#2c2d30";
+static const char col_normfg[]       = "#afb7c0";
+static const char col_selfg[]       = "#5da19f";
+static const char col_selbg[]        = "#000000";
+static const char col_selbord[]      = "#759abd";
