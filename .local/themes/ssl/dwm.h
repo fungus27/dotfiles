e62cb4a8d7cb018ab77419db393c5343950a8ff1
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-07-26 00:35:38 +0000

    add ssl theme

---
+++ .local/themes/ssl/dwm.h
@@ -0,0 +1,6 @@
+static const char col_normbg[]       = "#000000";
+static const char col_normbord[]       = "#000000";
+static const char col_normfg[]       = "#00b020";
+static const char col_selfg[]       = "#a52923";
+static const char col_selbg[]        = "#000000";
+static const char col_selbord[]      = "#00420c";
