b0c744752e295329b24348d828de26f60a2f861b
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-08-04 02:04:20 +0000

    add nord theme

---
+++ .local/themes/nord/dwm.h
@@ -0,0 +1,6 @@
+static const char col_normbg[]       = "#3b4252";
+static const char col_normbord[]       = "#475555";
+static const char col_normfg[]       = "#d8dee9";
+static const char col_selfg[]       = "#d8dee9";
+static const char col_selbg[]        = "#81a1c1";
+static const char col_selbord[]        = "#8fbcbb";
