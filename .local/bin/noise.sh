e89bcbae779ff8bf851952ecd64e74704744ed15
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-08-25 20:42:22 +0000

    change noise.sh

--- .local/bin/noise.sh
+++ .local/bin/noise.sh
@@ -2,25 +2,18 @@
 
 set -u
 set -e
-minutes=${1:-'120'}
-repeats=$(( minutes - 1 ))
+minutes=${1:-'120:00'}
 center=${2:-'2'}
 wave=${3:-'0.000333333'}
 noise='brown'
 len='01:00'
 
-if [ $minutes -eq 1 ] ; then
-     progress='--show-progress'
-else
-     progress='--no-show-progress'
-fi
-echo " ::  Please stand-by... sox will 'play' $noise noise for $minutes minute(s)."
+echo " ::  Please stand-by... sox will 'play' $noise noise for $minutes."
 
-play $progress -S  -c 2  --null  synth  $len  ${noise}noise  \
+play --show-progress -c 2  --null  synth  $minutes  ${noise}noise  \
      band -n $center 499               \
      tremolo $wave    43   reverb 19   \
      bass -11              treble -1   \
      vol     14dB                      \
-     repeat  $repeats
 
 exit 0
