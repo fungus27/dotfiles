364d8faf9e6bcb041a9ca3fe2c8a08fcc2af8bc7
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2023-07-14 14:02:41 +0000

    remove firefox

--- .mozilla/firefox/0gvy5t86.default/user.js
+++
@@ -1,9 +0,0 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("accessibility.typeaheadfind.enablesound", false);
-user_pref("accessibility.typeaheadfind.flashBar", 0);
-user_pref("browser.urlbar.placeholderName", "zupa");
-user_pref("identity.fxaccounts.enabled", false);
-user_pref("font.name.sans-serif.x-western", "Signika Negative");
-user_pref("font.name.serif.x-western", "Signika Negative");
-user_pref("font.name.monospace.x-western", "Greybeard 15px");
-user_pref("font.size.variable.x-western", 17);
